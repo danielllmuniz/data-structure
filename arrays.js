a7d73691142a9108dcb3086aed1245c4b59bf2ed
Daniel Muniz <munidaniel@gmail.com>
2024-06-01 19:40:09 -0300

    implementing more function for arrays

--- arrays.js
+++ arrays.js
@@ -65,6 +65,10 @@ console.log(entries.next().value)
 console.log(entries.next().value)
 console.log(entries.next().value)
 
+console.log('\nSum array values')
+const sumArr = arr.reduce((acc, curr) => acc += curr, 0)
+console.log(sumArr)
+
 console.log('\nMapping elements')
 const mappedArr = arr.map(el => el % 2 === 0)
 console.log(mappedArr)
@@ -79,9 +83,13 @@ console.log('\nFinding an element (it return only one element)')
 const findArr = arr.find(el => el > 2)
 console.log(findArr)
 
-console.log('\nFinding if exists any condition')
+console.log('\nTest if exists any condition')
 const someArr = arr.some(el => el ===2)
 console.log(someArr)
+
+console.log('\nTest if all elements match with condition')
+const everyArr = arr.every(el => el === 2)
+console.log(everyArr)
 
 console.log('\nSorting arr')
 const sortedArrDesc = arr.sort((a, b) => b - a)
@@ -89,8 +97,17 @@ console.log(sortedArrDesc)
 console.log(arr)
 arr.sort((a, b) => a - b)
 
+console.log('\nrevert array')
+console.log(arr.reverse())
+console.log(arr)
+console.log(arr.reverse())
+
 console.log('\nConverting to string')
 console.log(arr.toString())
+console.log(arr)
+
+console.log('\nConverting to string changing the signals')
+console.log(arr.join('-'))
 console.log(arr)
 
 console.log('\nFind using at')
@@ -98,3 +115,26 @@ console.log(arr.at(1))
 console.log(arr.at(-2))
 console.log(arr[1])
 console.log(arr[arr.length -2])
+
+console.log('\nFill positions')
+const arrForFill1 = [...arr]
+const arrForFill2 = [...arr]
+console.log(arrForFill1.fill(1))
+console.log(arrForFill1)
+console.log(arrForFill1.fill(2, 3))
+console.log(arr)
+
+
+console.log('\nCreate a new array with option')
+console.log(arr.with(2, 6)); // [1, 2, 6, 4, 5]
+console.log(arr); // [1, 2, 3, 4, 5]
+
+
+console.log('\nFlatting array')
+const multiArray = [1,2,3,[4,5,6,[7,7,[8,8,[9,9,]]]]]
+console.log(multiArray.flat())
+console.log(multiArray.flat(2))
+console.log(multiArray.flat(Infinity))
+
+console.log(multiArray.includes(3))
+console.log(multiArray.includes(8))
